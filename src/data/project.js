c1f7ec299463202c57dfe9b8e5cd7badbbddbc0a
Avik Mitra <avikmitra420@gmail.com>
2023-09-22 00:27:40 +0530

    Update project.js

--- src/data/project.js
+++ src/data/project.js
@@ -3,7 +3,7 @@ let projectsData = [
     id: 1,
     name: "Zomato-Clone",
     description:
-      "It's a food ordering website. It has many fetures like searching, sorting, filtering, pagination, adding items to cart and many more. Also it is an dynamic website and its responsive. For the frontend part of this project I used React, html, css, BootStrap and for the Backend portion I used Node Js and Express Js and for Database I used MongoDB. Also It is deployed on Render",
+      "It's a food-ordering website. It has many features like searching, sorting, filtering, pagination, adding items to a cart, and many more. Also, it is a dynamic website, and it's responsive. For the frontend part of this project, I used React, HTML, CSS, and BootStrap and for the Backend portion I used Node JS and Express JS and for the Database I used MongoDB. Also, It is deployed on Render.",
     github: "https://github.com/avIKjr/Zomato-Frontend",
     url: "https://zomatoclone-gul3.onrender.com/",
     image: "assets/zomato-website.JPG",
@@ -12,7 +12,7 @@ let projectsData = [
     id: 2,
     name: "Mentors Section",
     description:
-      "This is Mentors section of an website. I have worked in the front end part of it. I have used React, html, css, Bootstrap and also react-swiper to build it. It is fully Responsive and dynamic.",
+      "This is the Mentors section of the website. I have worked in the front-end part of it. I have used React, HTML, CSS, Bootstrap, and react-swiper to build it. It is fully Responsive and dynamic.",
     github: "https://github.com/avIKjr/Mentors-Section",
     url: null,
     image: "assets/mentors-section.jpeg",
@@ -21,7 +21,7 @@ let projectsData = [
     id: 3,
     name: "Search Function",
     description:
-      "This is an app with simple search functionality where you can search items by its name. I have used React, html, css, Bootstrap to make it.",
+      "This is an app with simple search functionality where you can search items by their names. I have used React, HTML, CSS, and Bootstrap to make it.",
     github: "https://github.com/avIKjr/search-api",
     url: null,
     image: "assets/Search functionality.JPG",
@@ -30,7 +30,7 @@ let projectsData = [
     id: 4,
     name: "Video Trailer Pop-Up",
     description:
-      "It's and simple video trailer website and I have used  html, css and javascript to make it.",
+      "It's a simple video trailer website and I have used  HTML, CSS, and javascript to make it.",
     github: "https://github.com/avIKjr/Video_Trailer_popup",
     url: null,
     image: "assets/video-trailer-pop-up.JPG",
@@ -39,7 +39,7 @@ let projectsData = [
     id: 5,
     name: "Digital Clock",
     description:
-      "It's a digital clock and I have used  html, css and javascript to make it.",
+      "It's a digital clock and I have used  HTML, CSS, and javascript to make it.",
     github: "https://github.com/avIKjr/Digital_Clock/tree/main/Digital%20Clock",
     url: null,
     image: "assets/digital-clock.JPG",
@@ -48,7 +48,7 @@ let projectsData = [
     id: 6,
     name: "Character Count",
     description:
-      "It's a application where you can see the count of the charecters that you will type in the given box. I have used  html, css and javascript to make it.",
+      "It's an application where you can see the count of the characters that you will type in the given box. I have used  HTML, CSS, and javascript to make it.",
     github: "https://github.com/avIKjr/Charecter_count",
     url: null,
     image: "assets/character-count.JPG",
@@ -57,7 +57,7 @@ let projectsData = [
     id: 7,
     name: "Multiplication Quiz App",
     description:
-      "Its a simple multiplication quiz app where you will begiven scopres based on the answers. I have used  html, css and javascript to make it.",
+      "It's a simple multiplication quiz app where you will be given scores based on the answers. I have used  HTML, CSS, and javascript to make it.",
     github: "https://github.com/avIKjr/Multiplication_App",
     url: null,
     image: "assets/multiplication-app.JPG",
